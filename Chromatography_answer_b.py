6f84ec154eb76537fe771b9a9f2261b30e324d33
s1sun <sean_s_sun@hotmail.com>
2024-06-24 02:19:45 -0700

    Update Chromatography_answer_b.py

--- Chromatography_answer_b.py
+++ Chromatography_answer_b.py
@@ -5,6 +5,14 @@ import chromatography
 if __name__ == '__main__':
     binary_file_path = 'scale/scale'
     csv_file_path = 'scale/scale.csv'
+    original_file_path = 'scale/scale_original.csv'
     
     # Call the chromatography functions to parse and save as CSV
     chromatography.parse_scale_binary(binary_file_path, csv_file_path)
+
+    # Compare the generated CSV with scale_original.csv
+    df1 = pd.read_csv(csv_file_path)
+    df2 = pd.read_csv(original_file_path)
+    diff = df1.compare(df2)
+    print("Differences between df1 and df2:")
+    print(diff)
